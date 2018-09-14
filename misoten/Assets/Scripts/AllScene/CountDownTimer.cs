fe678f17bb6891c60a3c5d04420731b2767371b7
MYCOMPUTER\th <th.fuchimoto@gmail.com>
2018-09-14 11:51:16 +0900

    ファイル構造の変更と、津野くんのキーインプット、北川のUnityちゃん表示の追加

---
+++ misoten/Assets/Scripts/AllScene/CountDownTimer.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// カウントダウンのスクリプト
+/// </summary>
+public class CountDownTimer : MonoBehaviour
+{
+	// 定数、一分
+	const int MINUTE = 60;
+
+	private float m_oldSeconds;					// 前回Update時の秒数
+	private float m_totalTime;					// トータル制限時間
+	private Text m_timerText;					// 制限時間のテキスト
+	[SerializeField] private int m_minute;		// 制限時間(分)
+	[SerializeField] private float m_seconds;	// 制限時間(秒)
+
+	/// <summary>
+	/// 開始関数
+	/// </summary>
+	private void Start()
+	{
+		// 前回Update時の秒数の初期化 
+		m_oldSeconds = 0.0f;
+
+		// 設定された時間(分)と時間(秒)を加算して保存
+		m_totalTime = (m_minute * MINUTE) + m_seconds;
+
+		// テキストのコンポーネント取得
+		m_timerText = GetComponentInChildren<Text>();
+	}
+
+	/// <summary>
+	/// 更新関数
+	/// </summary>
+	private void Update()
+	{
+		// 制限時間が0秒以下なら何もしない
+		if (m_totalTime <= 0.0f) { return; }
+		else
+		{
+			// 一旦トータルの制限時間を計測
+			m_totalTime = m_minute * MINUTE + m_seconds;
+			m_totalTime -= Time.deltaTime;
+
+			// 再設定
+			m_minute = (int)m_totalTime / MINUTE;
+			m_seconds = m_totalTime - m_minute * MINUTE;
+
+			// タイマー表示用UIテキストに時間を表示する
+			if ((int)m_seconds != (int)m_oldSeconds)
+			{
+				m_timerText.text = m_minute.ToString("00") + ":" + ((int)m_seconds).ToString("00");
+			}
+
+			// 前回の秒数の更新
+			m_oldSeconds = m_seconds;
+
+			// 制限時間以下になったらコンソールに『制限時間終了』という文字列を表示する
+			if (m_totalTime <= 0.0f) { Debug.Log("制限時間終了"); }
+		}
+	}
+}
---
+++ misoten/Assets/Scripts/AllScene/ParticleCallback.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// パーティクルの演出が終了したらオブジェクトを削除するコールバックスクリプト
+/// </summary>
+public class ParticleCallback : MonoBehaviour
+{
+	// メインのゲームオブジェクト
+    private GameObject m_owner;
+
+	// 指定されたオブジェクトを削除する為のもの
+	[SerializeField] private GameObject m_setObj;
+
+	/// <summary>
+	/// 開始関数
+	/// </summary>
+	private void Start()
+	{
+		// 処理内容は特に無し
+	}
+
+	/// <summary>
+	/// 更新関数
+	/// </summary>
+	private void Update()
+	{
+		// オブジェクトが「NULL」では無い時のみ
+		if (m_owner != null)
+		{
+            this.transform.position = m_owner.transform.position;
+        }
+    }
+
+	/// <summary>
+	/// オブジェクトがアクティブ時に呼ばれる
+	/// </summary>
+	private void OnEnable()
+	{
+		// パーティクルのコルーチン開始
+		StartCoroutine(ParticleWorking());
+	}
+
+	/// <summary>
+	/// パーティクルのコルーチンの処理
+	/// </summary>
+	/// <returns></returns>
+	IEnumerator ParticleWorking()
+	{
+		// パーティクルのコンポーネント取得
+		var particle = GetComponent<ParticleSystem>();
+
+        // パーティクルの演出中であればここで処理を止める
+        yield return new WaitWhile(() => particle.IsAlive());
+
+        // 演出が終わったらパーティクルとゲームオブジェクトを削除
+        Destroy(m_setObj);
+		Destroy(gameObject);
+    }
+
+	/// <summary>
+	/// オブジェクトの状態を取得
+	/// </summary>
+	/// <param name="gameObject">オブジェクト取得</param>
+    public void SetOwner(GameObject gameObject)
+	{
+        this.m_owner = gameObject;
+    }
+}
