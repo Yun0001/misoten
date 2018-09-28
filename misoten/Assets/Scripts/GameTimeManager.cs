e8977ae696cde03e1a85dd2211bf38dd09365cef
yun <ayaki.nieda@gmail.com>
2018-09-28 11:58:12 +0900

    残り時間：GameTimemanager追加

---
+++ misoten/Assets/Scripts/GameTimeManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimeManager : MonoBehaviour {
+
+    private float countTime;
+    private float timespeed;
+
+    // Use this for initialization
+    void Start () {
+        countTime = 90;
+        timespeed = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameTimer();
+        TimerSpeed();
+        TimeOver();
+	}
+
+    //時間管理
+    private void GameTimer()
+    {
+        //countTime -= Time.deltaTime; //スタートしてからの秒数を格納
+        GameObject.Find("TimeText").GetComponent<Text>().text = "残りタイム : " + countTime.ToString("F2"); //小数2桁にして表示
+    }
+
+    //時間加速
+    private void TimerSpeed()
+    {
+        CountSpeed();
+        countTime -= Time.deltaTime * timespeed;
+    }
+
+    private void CountSpeed()
+    {
+        //ToDo:仮置き
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            timespeed += 0.5f;
+            GameObject.Find("SpeedUpText").GetComponent<Text>().text = "スピードアップ:"+ timespeed.ToString();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            timespeed -= 0.5f;
+            GameObject.Find("SpeedUpText").GetComponent<Text>().text = "スピードダウン:"+ timespeed.ToString();
+        }
+
+        //通常スピード
+        if (timespeed == 1.0f)
+        {
+            GameObject.Find("SpeedUpText").GetComponent<Text>().text = " 通常スピード :" +timespeed.ToString();
+        }
+
+
+        //スピード限界
+        if (timespeed <= 0.5f)
+        {
+            timespeed = 0.5f;
+            GameObject.Find("SpeedUpText").GetComponent<Text>().text = "スピードダウン:" + timespeed.ToString();
+        }
+        if (timespeed >= 10.0f)
+        {
+            timespeed = 10.0f;
+            GameObject.Find("SpeedUpText").GetComponent<Text>().text = "スピードアップ:" + timespeed.ToString();
+        }
+    }
+
+    //時間切れゲーム終了
+    private void TimeOver()
+    {
+        if (countTime<=0)
+        {
+            countTime = 0;
+        }        
+    }
+    
+
+}
