d5ed4158b8b110957096bdb578c2367d13164ceb
kitagawakenta <susayxxx@gmail.com>
2018-09-28 09:18:24 +0900

    サウンドマネージャ追加（未完成）

--- misoten/Assets/Scripts/AllScene/SoundManager.cs
+++ misoten/Assets/Scripts/AllScene/SoundManager.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour {
+public class SoundManager : Singleton<SoundManager>
+{
+
+    public enum seName
+    {
+        seTest
+    }
+
+    public enum bgmName
+    {
+        bgmTest
+    }
+
+    [SerializeField]
+    private AudioClip[] seClip;
+
+    [SerializeField]
+    private AudioClip[] bgmClip;
+
+    private AudioSource[] audioSource;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+    {
+        audioSource[0] = gameObject.GetComponent<AudioSource>();
+        audioSource[0].clip = seClip[0];	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void PlaySE(seName sename)
+    {
+        audioSource[0].clip = seClip[(int)sename];
+        audioSource[0].Play();
+    }
+
+    public void PlayBGM(bgmName bgmname)
+    {
+        audioSource[0].clip = bgmClip[(int)bgmname];
+        audioSource[0].Play();
+    }
+
 }
