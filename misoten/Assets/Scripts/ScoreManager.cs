b962402b39d5aca0219c8bbf9e108e4ddcd89c8e
kitagawakenta <susayxxx@gmail.com>
2018-09-30 14:57:36 +0900

    加点する時に順位係数を掛けてから加点するように変更

--- misoten/Assets/Scripts/ScoreManager.cs
+++ misoten/Assets/Scripts/ScoreManager.cs
@@ -16,7 +16,7 @@ public class ScoreManager : Singleton<ScoreManager>
     /// 邪魔係数
     /// </summary>
     [SerializeField]
-    private float hindranceCoefficient;
+    private float hindranceCoefficient ;
 
     /// <summary>
     /// 時間係数
@@ -24,6 +24,10 @@ public class ScoreManager : Singleton<ScoreManager>
     [SerializeField]
     private float timeCoefficient;
 
+    /// <summary>
+    /// 順位
+    /// </summary>
+    private int[] playerRank = new int[playerNum];
 
     // Use this for initialization
     void Start()
@@ -31,27 +35,104 @@ public class ScoreManager : Singleton<ScoreManager>
         for (int i = 0; i < playerNum; i++)
         {
             scoreText[i] = playerScore[i].GetComponent<Text>();
-            scoreText[i].text = "Score:"+ playerScore[i].gameObject.GetComponent<ScoreCount>().GetScore().ToString();
+            playerRank[i] = 1;
+            scoreText[i].text = "Score:"+ playerScore[i].gameObject.GetComponent<ScoreCount>().GetScore().ToString()+"/"+playerRank[i].ToString();
+
         }
-            
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     
     public void AddScore(int pID, int score)
     {
-        playerScore[pID].gameObject.GetComponent<ScoreCount>().AddScore(score);
-        scoreText[pID].text = "Score:" + playerScore[pID].gameObject.GetComponent<ScoreCount>().GetScore().ToString();
+        playerScore[pID].gameObject.GetComponent<ScoreCount>().AddScore(CalcAddScore(pID, score));
+        int oldRank = playerRank[pID];
+        DecisionPlayerRank();
+        if (oldRank != playerRank[pID])
+        {
+           
+        }
+        else
+        {
+            scoreText[pID].text = "Score:" + playerScore[pID].gameObject.GetComponent<ScoreCount>().GetScore().ToString() + "/" + playerRank[pID].ToString();
+        }
+        for (int i = 0; i < playerNum; i++)
+        {
+            scoreText[i].text = "Score:" + playerScore[i].gameObject.GetComponent<ScoreCount>().GetScore().ToString() + "/" + playerRank[i].ToString();
+        }
+
     }
 
     public void SubScore(int pID, int score)
     {
         playerScore[pID].gameObject.GetComponent<ScoreCount>().SubScore(score);
-        scoreText[pID].text = "Score:" + playerScore[pID].gameObject.GetComponent<ScoreCount>().GetScore().ToString();
+        DecisionPlayerRank();
+        scoreText[pID].text = "Score:" + playerScore[pID].gameObject.GetComponent<ScoreCount>().GetScore().ToString() + "/" + playerRank[pID].ToString();
+    }
+
+    /// <summary>
+    /// 順位決定
+    /// </summary>
+    private void DecisionPlayerRank()
+    {
+        int[] workArray = new int[playerNum];
+        InitArray(workArray);       // 作業用配列初期化
+        BubbleSort(workArray);   // バブルソート
+        SetRank(workArray);      // 順位セット
+    }
+
+    /// <summary>
+    ///  作業用配列初期化
+    /// </summary>
+    /// <param name="workarray"></param>
+    private void InitArray(int[] workarray)
+    {
+        for (int i = 0; i < playerNum; i++)
+        {
+            workarray[i] = playerScore[i].gameObject.GetComponent<ScoreCount>().GetScore();
+        }
+    }
+
+    /// <summary>
+    /// バブルソート
+    /// </summary>
+    /// <param name="workarray"></param>
+    private void BubbleSort(int[] workarray)
+    {
+        for (int i = 0; i < playerNum - 1; i++)
+        {
+            for (int j = playerNum - 1; j > i; j--)
+            {
+                if (workarray[j - 1] < workarray[j])
+                {
+                    int work = workarray[j - 1];
+                    workarray[j - 1] = workarray[j];
+                    workarray[j] = work;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    ///  順位セット
+    /// </summary>
+    /// <param name="workarray"></param>
+    private void SetRank(int[] workarray)
+    {
+        for (int i = 0; i < playerNum; i++)
+        {
+            for (int j = 0; j < playerNum; j++)
+            {
+                if (playerScore[i].gameObject.GetComponent<ScoreCount>().GetScore() == workarray[j])
+                {
+                    playerRank[i] = j + 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    private int CalcAddScore(int pID, int score)
+    {
+        //　後で時間係数も計算に加える
+        return score * playerRank[pID];
     }
 }
