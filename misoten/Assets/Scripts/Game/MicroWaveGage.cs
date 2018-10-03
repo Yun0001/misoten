ef200d4cfbc206b6260e646de8f024a3836a6dba
LAPTOP-4JH5TPJC\ohs50248 <reiji19no7664@icloud.com>
2018-10-03 21:41:06 +0900

    横ゲージと円ゲージのスクリプト修正&ゲージのPrefab化完了

--- misoten/Assets/Scripts/Game/MicroWaveGage.cs
+++ misoten/Assets/Scripts/Game/MicroWaveGage.cs
@@ -9,8 +9,28 @@ using GamepadInput;
 /// </summary>
 public class MicroWaveGage : MonoBehaviour
 {
+	/// <summary>
+	/// ゲージの種類
+	/// </summary>
+	private enum EGageType
+	{
+		SideGage = 0,
+		CircleGage,
+	};
+
+	// ゲージの種類の列挙型変数
+	[SerializeField] EGageType gageType;
+
+	// ゲージ処理切り替え
+	bool change = true;
+
 	// ゲージのGUI
 	private Slider slider;
+	private Image image;
+
+	// ゲージの進み具合
+	[SerializeField] [Range(0.0f, 100.0f)]
+	private float gageSpd;
 
 	/// <summary>
 	/// 開始関数
@@ -18,6 +38,7 @@ public class MicroWaveGage : MonoBehaviour
 	void Start()
 	{
 		slider = GetComponent<Slider>();
+		image = GetComponent<Image>();
 	}
 
 	/// <summary>
@@ -25,11 +46,45 @@ public class MicroWaveGage : MonoBehaviour
 	/// </summary>
 	void Update()
 	{
-		slider.value += 0.01f;
-	}
-
-	public void Hoge()
-	{
-		Debug.Log(slider.value);
+		// ゲージ処理の切り替え管理
+		switch (change)
+		{
+			case true:
+				// ゲージの種類管理
+				// ゲージが増えていく
+				switch(gageType)
+				{
+					case EGageType.SideGage:
+						if (slider.value < 100.0f) { slider.value += gageSpd; }
+						else { change = false; }
+						break;
+					case EGageType.CircleGage:
+						if (image.fillAmount < 1.0f) { image.fillAmount += gageSpd; }
+						else { change = false; }
+						break;
+					default:
+						break;
+				}
+				break;
+			case false:
+				// ゲージの種類管理
+				// ゲージが減っていく
+				switch (gageType)
+				{
+					case EGageType.SideGage:
+						if (slider.value > 0.0f) { slider.value += -gageSpd; }
+						else { change = true; }
+						break;
+					case EGageType.CircleGage:
+						if (image.fillAmount > 0.0f) { image.fillAmount += -gageSpd; }
+						else { change = true; }
+						break;
+					default:
+						break;
+				}
+				break;
+			default:
+				break;
+		}
 	}
 }
