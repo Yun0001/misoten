0979fc162465cb140e944e7bf41fd80bbbb7198c
kitagawakenta <susayxxx@gmail.com>
2018-10-09 15:27:24 +0900

    焼き台がゲージを生成するようにした。生成後ゲージの位置とスケールを調整

---
+++ misoten/Assets/Scripts/Game/Grilled.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grilled : MonoBehaviour {
+
+    public enum GrilledState
+    {
+        unused,
+        inCcoking
+    }
+
+    [SerializeField]
+    private GameObject sideGagePrefab;
+
+    private GameObject sideGage;
+
+    [SerializeField]
+    private float COOKING_TIME;
+
+    private float cookingTime;
+
+    private GrilledState grilledStatus;
+
+	// Use this for initialization
+	void Awake () {
+        sideGagePrefab = (GameObject)Resources.Load("Prefabs/UI/SideGage");
+        grilledStatus = GrilledState.unused;
+        cookingTime = 0;
+        sideGage = Instantiate(sideGagePrefab, transform.position, Quaternion.identity);
+        Vector3 pos = transform.position;
+        Vector3 scale = new Vector3(0.1f, 0.3f, 1);
+        sideGage.transform.position = pos;
+        sideGage.transform.localScale = scale;
+        sideGage.SetActive(false);
+    }
+
+    /// <summary>
+    /// 調理開始
+    /// </summary>
+    public void StartCooking()
+    {
+        grilledStatus = GrilledState.inCcoking;
+        cookingTime = COOKING_TIME;
+        sideGage.SetActive(true);
+    }
+}
