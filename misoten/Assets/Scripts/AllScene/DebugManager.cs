556725a6997f3917fc8de04e66fd36ae243a09e2
yun <ayaki.nieda@gmail.com>
2018-10-15 11:40:07 +0900

    DebugManager.cs追加：ESC終了処理

---
+++ misoten/Assets/Scripts/AllScene/DebugManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Exit();
+    }
+
+    /// <summary>
+    /// exe終了処理
+    /// </summary>
+    private void Exit()
+    {        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Exit");
+            Application.Quit();
+        }
+    }
+
+    
+    //private void VisualArea()
+    //{
+    //}
+
+    //private void VisualLog()
+    //{
+    //}
+
+
+}
