fd331a876194b6a849ccb735861c2085b052ed04
MYCOMPUTER\th <th.fuchimoto@gmail.com>
2018-10-23 17:03:45 +0900

    3dnewscene add model , title scene white out next scene load gamescene

---
+++ misoten/Assets/Scripts/BillBoard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+
+    void Update()
+    {
+        Vector3 camPosition = Camera.main.transform.position;
+        camPosition.x = transform.position.x;
+        transform.LookAt(camPosition);
+    }
+
+}
---
+++ misoten/Assets/Scripts/Title/ShopAnimEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopAnimEvent : MonoBehaviour {
+
+    private GameObject _canvasWO;
+    
+	void Start () {
+        _canvasWO = transform.GetChild(0).GetChild(0).gameObject;
+    }
+
+    void IsOpened()
+    {
+        _canvasWO.GetComponent<WhiteOut>().OnWhiteOut();
+    }
+
+}
