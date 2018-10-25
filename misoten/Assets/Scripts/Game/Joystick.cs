aed6b087344edb31f60a871b7889ad35b40ad7c1
kitagawakenta <susayxxx@gmail.com>
2018-10-25 11:08:58 +0900

    茹で料理のスティックの問題を修正

--- misoten/Assets/Scripts/Game/Joystick.cs
+++ misoten/Assets/Scripts/Game/Joystick.cs
@@ -17,7 +17,7 @@ public class Joystick : MonoBehaviour
 	private GameObject stickObj;
 
 	//スティックが動く範囲の半径
-	[SerializeField, Range(0.0f, 100.0f)]
+	[SerializeField, Range(0.0f, 1000.0f)]
 	private float radius;
 
 	// プレイヤーID
@@ -38,37 +38,51 @@ public class Joystick : MonoBehaviour
 	// プレイヤー
 	private Player player;
 
+    Vector2 imagePOS;
+
 	// ラジアン
 	private float radian = 0.0f;
 
-	// ---------------------------------------------
 
-	/// <summary>
-	/// 開始関数
-	/// </summary>
-	void Start()
+
+    // ---------------------------------------------
+
+    /// <summary>
+    /// 開始関数
+    /// </summary>
+    void Start()
 	{
 		//スティックを生成する必要があれば生成し、位置を中心に設定
 		stickObj.transform.localPosition = Vector3.zero;
 
 		// コンポーネント取得
 		player = GameObject.Find(nameObj[playerId]).gameObject.GetComponent<Player>();
-	}
+
+
+
+    }
 
 	/// <summary>
 	/// 更新関数
 	/// </summary>
 	void Update()
 	{
-		// スティックを動かすと座標変換される
-		RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(),
-			new Vector2(Input.GetAxis(player.GetInputXAxisName()), -Input.GetAxis(player.GetInputYAxisName())), null, out imagePos);
 
-		// 位置の更新
-		stickObj.transform.localPosition = imagePos;
+        // 現在のワールド座標をスクリーン座標に変換
+        Vector3 Position;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(GetComponent<RectTransform>(), gameObject.transform.position, null, out Position);
 
-		// 移動場所が設定した半径を超えてる場合は制限内に抑える
-		if (Vector3.Distance(Vector3.zero, stickObj.transform.localPosition) > radius)
+        //スティックのベクトルに現在のスクリーン座標を加算して計算
+        // スティックを動かすと座標変換される
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(),
+			new Vector2(Input.GetAxis(player.GetInputXAxisName()) + Position.x, -Input.GetAxis(player.GetInputYAxisName())+ Position.y), null, out imagePos);
+
+
+        // 位置の更新
+        stickObj.transform.localPosition = imagePos;
+
+        // 移動場所が設定した半径を超えてる場合は制限内に抑える
+        if (Vector3.Distance(Vector3.zero, stickObj.transform.localPosition) > radius)
 		{
 			// 角度計算
 			float radian = Mathf.Atan2(stickObj.transform.localPosition.y, stickObj.transform.localPosition.x);
@@ -76,5 +90,12 @@ public class Joystick : MonoBehaviour
 			// 円上にXとYを設定
 			stickObj.transform.localPosition = new Vector3(radius * Mathf.Cos(radian), radius * Mathf.Sin(radian), 0.0f);
 		}
-	}
+
+        //プレイヤーの上に表示
+        Vector3 pos = player.transform.position;
+        pos.y += 2;
+        gameObject.transform.position = pos;
+
+        stickObj.transform.localPosition += player.gameObject.transform.position;
+    }
 }
