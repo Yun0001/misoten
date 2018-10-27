5120f625501d80563f6b566219996e9432619fbd
MYCOMPUTER\th <th.fuchimoto@gmail.com>
2018-10-27 15:09:52 +0900

    add nabe model , new layout

---
+++ misoten/Assets/Scripts/Models/pan/CookWareAnimCtrl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookWareAnimCtrl : MonoBehaviour {
+
+    private Animator _animator;
+    private bool isAnimation = false;
+
+    public bool isLooping = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        OneAction();
+        LoopAction();
+
+        _animator.SetBool("isLooping", isLooping);
+
+    }
+
+    void OneAction()
+    {
+        if (isAnimation)
+        {
+            isAnimation = false;
+            _animator.Play("action");
+        }
+    }
+
+    void LoopAction()
+    {
+        if (isLooping)
+        {
+            _animator.Play("loopAction");
+        }
+    }
+
+}
