2f4da415a19889976f0d26d4c64e0bb6fdc2d09c
LAPTOP-4JH5TPJC\ohs50248 <reiji19no7664@icloud.com>
2018-11-16 11:06:04 +0900

    AlienのオーダーのUIを更新

---
+++ misoten/Assets/Scripts/Result/AlienResultAnimation.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// エイリアンのリザルト用アニメーション
+/// </summary>
+public class AlienResultAnimation : MonoBehaviour
+{
+	private enum EAlienPattern
+	{
+		MARTIAN = 0,
+		MERCURY,
+		VENUSIAN,
+		MAX
+	}
+
+	[SerializeField]
+	private EAlienPattern alienPattern;
+
+	private const int ANIMATION_NUM = 4;// アニメーションの数
+
+	[SerializeField, Range(1.0f, 100.0f)]
+	private float oneAnimPatternSwitchTime;
+
+	private float oneAnimPatternTime;
+
+	private float countAnimTime = 0;
+
+	private string[] textureName1 = { "Textures/Alien/Wait/Martian/1", "Textures/Alien/Satisfaction/Martian/1", "Textures/Alien/Wait/Martian/1", "Textures/Alien/Satisfaction/Martian/1" };
+	private string[] textureName2 = { "Textures/Alien/Wait/Mercury/1", "Textures/Alien/Satisfaction/Mercury/1", "Textures/Alien/Wait/Mercury/1", "Textures/Alien/Satisfaction/Mercury/1" };
+	private string[] textureName3 = { "Textures/Alien/Wait/Venusian/1", "Textures/Alien/Satisfaction/Venusian/1", "Textures/Alien/Wait/Venusian/1", "Textures/Alien/Satisfaction/Venusian/1" };
+
+	// スプライト
+	public Sprite[,,] sprite = new Sprite[(int)EAlienPattern.MAX, 2, ANIMATION_NUM];
+
+	private int animID = 0;
+
+	void Awake()
+	{
+		oneAnimPatternTime = oneAnimPatternSwitchTime / ANIMATION_NUM;
+
+		// 待機・歩き・怒り・満足画像ロード
+		switch (alienPattern)
+		{
+			case EAlienPattern.MARTIAN:
+				AnimationSpriteLoad(0, textureName1);
+				break;
+			case EAlienPattern.MERCURY:
+				AnimationSpriteLoad(1, textureName2);
+				break;
+			case EAlienPattern.VENUSIAN:
+				AnimationSpriteLoad(2, textureName3);
+				break;
+			default: break;
+		}
+	}
+
+	/// <summary>
+	/// 更新関数
+	/// </summary>
+	void Update ()
+	{
+		if (countAnimTime > oneAnimPatternTime)
+		{
+			countAnimTime = 0;
+			animID++;
+			if (animID > ANIMATION_NUM - 1) animID = 0;
+			ChangeSprite();
+		}
+		countAnimTime += 1.0f;
+	}
+
+	private void ChangeSprite() => GetComponent<SpriteRenderer>().sprite = sprite[(int)alienPattern, 0, animID];
+
+	private void AnimationSpriteLoad(int id, string[] name)
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			for (int k = 0; k < 4; k++)
+			{
+				sprite[id, i, k] = Resources.Load<Sprite>(name[k]);
+			}
+		}
+	}
+
+}
--- misoten/Assets/Scripts/Result/MenuMove.cs
+++ misoten/Assets/Scripts/Result/MenuMove.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuMove : MonoBehaviour {
+public class MenuMove : MonoBehaviour
+{
+	// インスペクター上で設定可能
+	// ---------------------------------------------
 
-	// Use this for initialization
-	void Start () {
-		
+	// 指定終点座標
+	[SerializeField]
+	private Vector3 endPos;
+
+	// 移動時間
+	[SerializeField]
+	private float moveTime;
+
+	// ---------------------------------------------
+
+	// 終点座標ID
+	private bool setEndPositionFlag = true;
+
+	// 時間更新用
+	private float timeAdd = 0.0f;
+
+	// 予定時間を割る用
+	private float rate = 0.0f;
+
+	/// <summary>
+	/// 開始関数
+	/// </summary>
+	void Start ()
+	{
+		// 終点座標IDの初期化
+		setEndPositionFlag = true;
+
+		// 時間更新用、予定時間を割る用の初期化
+		timeAdd = rate = 0.0f;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	/// <summary>
+	/// 更新関数
+	/// </summary>
+	void Update ()
+	{
+		if(setEndPositionFlag)
+		{
+			// 時間更新
+			timeAdd += Time.deltaTime;
+
+			// 予定時間を割る
+			rate = timeAdd / moveTime;
+
+			// 終点座標に到着
+			if (timeAdd > moveTime) { setEndPositionFlag = false; timeAdd = 0.0f; }
+			transform.position = Vector3.Lerp(new Vector3(-6.5f, 19.5f, 0.0f), endPos, rate);
+		}
 	}
 }
