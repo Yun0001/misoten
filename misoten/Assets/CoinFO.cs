3ace6c44fedfe0ea2fede6c1d7eb15d1bb7edc1e
MYCOMPUTER\th <th.fuchimoto@gmail.com>
2018-11-17 20:03:41 +0900

    update title , add new fade out

---
+++ misoten/Assets/CoinFO.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class CoinFO : MonoBehaviour {
+
+    [SerializeField] private bool isStartingCoinFO = false;
+    [SerializeField, Range(1.0f, 5.0f)] private float fadeSpeed = 3.0f;
+
+    private GameObject _coin;
+
+	// Use this for initialization
+	void Start () {
+        _coin = this.transform.GetChild(0).gameObject;// GameObject.Find("coinFO");
+        _coin.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isStartingCoinFO)
+        {
+            //this.transform.GetChild(0).gameObject.SetActive(true);
+            _coin.SetActive(true);
+            _coin.transform.position -= transform.right * fadeSpeed * Time.deltaTime;
+
+            if (_coin.transform.position.x<=-18.5f)
+            {
+                SceneManager.LoadScene("Scenes/Result");
+                //SceneManagerScript.GetInstance().LoadNextScene();
+            }
+        }
+
+    }
+}
---
+++ misoten/Assets/Scripts/Title/titleLogoAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class titleLogoAnimation : MonoBehaviour {
+
+    private RectTransform _titleLogo;
+    Vector3 _target = new Vector3(0, -0.5f, 0);
+    [SerializeField, Range(1.0f, 10.0f)] float slideSpeed = 5.0f;
+
+    private GameObject _canvasWIO;
+
+    void Start()
+    {
+        _titleLogo = GameObject.Find("titleLogo").GetComponent<RectTransform>();
+
+        _canvasWIO = GameObject.Find("WIO");
+    }
+
+    void Update()
+    {
+        
+
+        if (_titleLogo.localPosition.y <= 10.0f)
+        {
+            _titleLogo.localPosition = new Vector3(0, 10.0f, 0);
+            _canvasWIO.GetComponent<WhiteIO>().OnWhiteIn();
+        }
+        else
+        {
+            float t = Time.deltaTime / slideSpeed;
+            _titleLogo.localPosition = Vector3.Slerp(this.transform.localPosition, _target, t);
+        }
+
+    }
+
+}
