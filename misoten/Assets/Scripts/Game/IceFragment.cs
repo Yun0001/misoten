e1abe9e8bb68db7bf82fd86b5448f80e195b059b
LAPTOP-4JH5TPJC\ohs50248 <reiji19no7664@icloud.com>
2018-11-19 11:03:14 +0900

    アイスピックエフェクト修正

--- misoten/Assets/Scripts/Game/IceFragment.cs
+++ misoten/Assets/Scripts/Game/IceFragment.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class IceFragment : MonoBehaviour
 {
+	[SerializeField]
+	private int id;
+
 	private IceBoxMiniGame iceBoxMiniGame;
 
 	// Use this for initialization
 	void Start ()
 	{
-		iceBoxMiniGame = GameObject.Find("GameSceneManager/PauseManager/Stage/cookwares/iceboxes/icebox1/IceBoxMiniGame").gameObject.GetComponent<IceBoxMiniGame>();
+		if(id == 0) { iceBoxMiniGame = GameObject.Find("GameSceneManager/PauseManager/Stage/cookwares/iceboxes/icebox1/IceBoxMiniGame").gameObject.GetComponent<IceBoxMiniGame>(); }
+		else { iceBoxMiniGame = GameObject.Find("GameSceneManager/PauseManager/Stage/cookwares/iceboxes/icebox2/IceBoxMiniGame").gameObject.GetComponent<IceBoxMiniGame>(); }
 	}
 	
 	// Update is called once per frame
