27324bb8321fdcb7391cc8a557eedd2ab1a0699d
kitagawakenta <susayxxx@gmail.com>
2018-11-20 10:03:55 +0900

    調理を終えると氷エフェクトが止まるように

--- misoten/Assets/Scripts/Eatoy.cs
+++ misoten/Assets/Scripts/Eatoy.cs
@@ -4,35 +4,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class Eatoy : MonoBehaviour {
-
+public class Eatoy : MonoBehaviour
+{
+    /// <summary>
+    /// イートイカラー
+    /// </summary>
     public enum EEatoyColor { None, Yellow, Orange, Red, Purple, Bule, Green, ColorMax }
+
+    /// <summary>
+    /// イートイカラー
+    /// </summary>
     [SerializeField]
     private EEatoyColor eatoyColor;
 
+    /// <summary>
+    /// イートイポイント
+    /// </summary>
     [SerializeField]
     private int eatoyPoint;
 
+    /// <summary>
+    /// 凍っている状態か
+    /// </summary>
     [SerializeField]
     private bool isIcing;
+
+    /// <summary>
+    /// アイスエフェクト
+    /// </summary>
+    [SerializeField]
+    private ParticleSystem iceEffect;
 
 
     public void Init(int colorID, Sprite eatoySprite)
     {
         eatoyPoint = 0;
         isIcing = true;
-
+        
         // enum設定
         eatoyColor = (EEatoyColor)Enum.ToObject(typeof(EEatoyColor), colorID);
 
         // スプライト設定
         GetComponent<SpriteRenderer>().sprite = eatoySprite;
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 0.3f);
     }
 
+    /// <summary>
+    ///解凍
+    /// </summary>
+    public void Thawing()
+    {
+        isIcing = false;
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        iceEffect.Stop();
+    }
 
-    public void Thawing() => isIcing = false;
-
-    public void AddPoint(int p) => eatoyPoint += p;
+    public int AddPoint(int p) => eatoyPoint += p;
 
     public void SubPoint(int p) => eatoyPoint = Mathf.Max(0, eatoyPoint - p);
 
