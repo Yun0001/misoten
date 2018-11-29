002da2b37ded94ab312a1d40b529788ab440cc72
MYCOMPUTER\th <th.fuchimoto@gmail.com>
2018-11-29 19:10:42 +0900

    タイトルに新しい待機のアニメーション追加

---
+++ misoten/Assets/Scripts/Title/StoryBoardCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryBoardCtrl : MonoBehaviour {
+
+    private bool isCompleteStoryAnimation = false;
+    private Animator _animator;
+    private GameObject _canvasWIO;
+
+    void Start() {
+        _animator = this.GetComponent<Animator>();
+        _canvasWIO = GameObject.Find("WIO");
+    }
+
+    void Update() {
+    }
+
+    void CompleteStoryAnimation()
+    {
+        _canvasWIO.GetComponent<WhiteIO>().OnSeWhiteIn();
+    }
+
+    public bool GetIsCompleteStartAnimation() => isCompleteStoryAnimation;
+
+    public void SetIsStartStory(bool b) => _animator.SetBool("isStartStory", b);
+
+    public void SetEnabled(bool b) => this.GetComponent<SpriteRenderer>().enabled = b;
+
+    public void OnCompleteStoryAnimation() => isCompleteStoryAnimation = true;
+
+
+}
